5748ee9895edfbe4ddcff9d1455a4b88d50db327
Florian Piel <f.piel@campus.tu-berlin.de>
2026-09-15 18:23:30 +0200

    Simplified animations using css

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
-const canvas = document.querySelector('.canvas');
-const block = canvas.querySelector('.block');
-const player = canvas.querySelector('.player');
-const canvasWidth = parseInt(window.getComputedStyle(canvas)['width'].split('px')[0]);
-const canvasHeight = parseInt(window.getComputedStyle(canvas)['height'].split('px')[0]);
-const blockWidth = parseInt(window.getComputedStyle(block)['width'].split('px')[0]);
-const playerHeight = parseInt(window.getComputedStyle(player)['height'].split('px')[0]);
-const stepSize = 5;
-function moveBlock() {
-  const currentTranslate = parseInt(block.getAttribute('style').split('left: ')[1].split('px')[0]);
-  let newTranslate = currentTranslate - stepSize;
-  if (newTranslate < 0 - blockWidth) {
-    newTranslate = canvasWidth + blockWidth;
-  }
-  block.setAttribute('style', `left: ${newTranslate}px;`);
-  setTimeout(moveBlock, 10);
-}
-
-setTimeout(moveBlock, 10);
-const jumpHeight = 2;
-const tickLimit = 80;
+const player = document.querySelector('.player');
+const jumpLengthInMillis = 1600;
 let inJump = false;
-function movePlayer(tick = 0, direction = 1) {
-    if (tick === tickLimit * 2 && direction === -1) {
-        inJump = false;
-        return;
-    }
-    const currentTranslate = parseInt(player.getAttribute('style').split('top: ')[1].split('px')[0]);
-    if (tick === tickLimit) {
-        direction = -1;
-    }
-    const newTranslate = currentTranslate - jumpHeight * direction;
-    player.setAttribute('style', `top: ${newTranslate}px;`);
-    setTimeout(() => movePlayer(tick + 1, direction), 10);
+function removeJump() {
+    player.classList.remove('jump');
+    inJump = false;
+    //Forces the browser to reflow reference:https://stackoverflow.com/a/63561659
+    player.offsetWidth;
 }
 
 addEventListener('keydown', (e) => {
     if(e.key === " ") {
         if (!inJump) {
             inJump = true;
-            movePlayer();
+            player.classList.add('jump');
+            setTimeout(removeJump, 1600);
         }
     }
 })
