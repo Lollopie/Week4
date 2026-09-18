17175f2b5f935dd8310dd51bee28d8d3df1e3280
Valerija <vbockaj@gmail.com>
2026-09-18 18:47:49 +0200

    Main menu button edited

--- index.js
+++ index.js
@@ -192,6 +192,7 @@ function resetGame() {
     isPaused = false;
 
     scoreField.textContent = 'Score: 0';
+    scoreField.classList.remove('hidden');
     gameOverCard.classList.add('hidden');
     menu.classList.add('hidden');
     playerJumping.classList.remove('jump', 'paused');
@@ -215,6 +216,7 @@ function returnToMainMenu() {
 
     mainMenuContainer.classList.remove('hidden');
     isMainMenuOpen = true;
+    scoreField.classList.add('hidden');
 }
 
 const playButton = document.querySelector('.play-button');
